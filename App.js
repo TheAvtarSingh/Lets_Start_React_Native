47cb82b93de018757e16f42c0b67d8b663977e47
TheAvtarSingh <singha2k2@gmail.com>
2023-04-11 11:48:17 +0530

    Components

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 
 import {  Text, View,Button } from 'react-native';
+import PersonalData from './components/PersonalData';
 
 const date = "5/4/2023"
 const data  = {fontSize:30}
@@ -13,9 +14,26 @@ export default function App() {
       <Text>Adding View - Text</Text>
       <Text>{textadded}</Text>
       <Button title='Press Here'></Button>
+      <Text> ----------- Personal Data Component (Different File)----------</Text>
+      <PersonalData />
+      <Text> ----------- Company Data Component----------</Text>
+      <CompanyData />
     </View>
   );
 }
 
+
+const CompanyData  =() =>{
+ 
+  return (
+    <View >
+    <Text >Comapny</Text>
+    <Text>Salary</Text>
+    <Text>Name</Text>
+    <Text>Age</Text>
+    <Button title='Press Here'></Button>
+  </View>
+  );
+}
 
 
